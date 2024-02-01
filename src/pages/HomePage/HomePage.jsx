65e79fbece285b4f9d703ad3540180a3a444ee2d
Zachary Mitchell <masspike84@yahoo.com>
2024-02-01 12:54:05 -0500

    Add HomePage copy

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,8 +2,9 @@ export default function HomePage() {
 	return (
 		<>
 			<div>
-				<h1>Welcome to the Home Page</h1>
-				<p>This is the home page content.</p>
+				<h1>Zachary Swan Mitchell</h1>
+				<p>Results-driven Full Stack Developer with a commitment to sustainability. Transitioned from a dynamic role as a Development Coordinator to achieve proficiency in JavaScript, Node.js, React, and more through General Assembly. Demonstrated success in coordinating development activities, leading support teams, and catalyzing positive change through technology. Dedicated to bringing seasoned technical expertise and project management skills to innovative software development initiatives.
+</p>
 			</div>
 		</>
 	);
