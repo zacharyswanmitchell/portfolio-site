24b0831893517173ccc3203d969a78b21c92c12e
Zachary Mitchell <masspike84@yahoo.com>
2024-02-01 12:57:32 -0500

    Add AboutPage copy

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,8 +1,37 @@
 export default function AboutPage() {
 	return (
 		<div>
-			<h1>About</h1>
-			<p>This is the about page content.</p>
+			<h1>About Me</h1>
+			<p>
+				Hello, I'm Zachary Mitchell, a passionate and recently graduated
+				full-stack developer on a journey to transform my career. My path to web
+				development started with a career shift – I was a Technical Support
+				Manager until May, facing the challenges of a job search. Following the
+				recommendation of a close friend who had successfully completed the
+				Software Engineering course at General Assembly, I decided to take the
+				leap into the world of coding. Now, I'm eagerly seeking new
+				opportunities in the field. ​ My educational background includes studies
+				in Pastoral Ministries at Southeastern University, where I spent time
+				volunteering in various ministries. While I cherish the experiences
+				gained, I realized my true passion lies in technology and creating
+				meaningful solutions. ​ In the realm of development, I find joy in
+				crafting dynamic and responsive applications using JavaScript, Node.js,
+				Express, React, Python, and Django. While I'm just embarking on my job
+				search, I've already accumulated a repertoire of class projects that
+				showcase my skills and dedication. ​ When it comes to problem-solving, I
+				have a unique approach – I review code meticulously and even
+				"rubberduck" it to my cats to uncover bugs. Collaboration is at the
+				heart of my process, having worked on two group projects where I learned
+				the ins and outs of Git and version control. ​ What drives me in the
+				field of web development is the prospect of creating solutions that
+				matter. I aspire to contribute to a workplace that values people and
+				makes a positive impact on the world. ​ Among my notable projects, I
+				take pride in my work on projects like reHomr and Disc tracker, each
+				offering a glimpse into my evolving skill set. You can find links to
+				these projects and more in my portfolio. ​ I'm excited about the
+				potential to contribute my skills and continue growing in the world of
+				web development. Let's connect and explore the possibilities together!
+			</p>
 		</div>
 	);
 }
