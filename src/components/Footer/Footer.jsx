eceac1a730ec88b04919f61572c5e50871a32bec
Zachary Mitchell <masspike84@yahoo.com>
2024-02-05 17:26:45 -0500

    Add footer details and icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,29 @@
 import "./Footer.css";
 import Container from "react-bootstrap/Container";
-import Link from "react-router-dom";
+import { Link } from "react-router-dom";
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
 export default function Footer() {
-    return (
-        <Container fluid>
-            <footer className="footer">
-                <p>
-                    &copy; 2021 Zachary Swan Mitchell. All rights reserved.
-                </p>
-            </footer>
-        </Container>
-    );
+	return (
+        <footer>
+            <Container fluid>
+            <div className="socials">
+                            <a href="https://www.linkedin.com/in/zacharysmitchell/" target="_blank" rel="noreferrer">
+                                <FaLinkedin className="social-icon" />
+                            </a>
+                            <a href="https://github.com/zacharyswanmitchell" target="_blank" rel="noreferrer">
+                                <FaGithub className="social-icon" />
+                            </a>
+                </div>
+                <div>
+				<p>&copy; 2024 Zachary Swan Mitchell. All rights reserved.</p>
+                </div>
+                <div className="links">
+                    <Link to="/" className="footer-links">Home</Link>&nbsp; | &nbsp;
+                    <Link to="/about" className="footer-links">About</Link>&nbsp; | &nbsp;
+                    <Link to="/projects" className="footer-links">Projects</Link>
+                </div>
+			</Container>
+		</footer>
+	);
 }
