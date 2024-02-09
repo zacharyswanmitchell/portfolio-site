ee347375fa03608cfc9ac57497d6341d84435597
Zachary Mitchell <masspike84@yahoo.com>
2024-02-09 12:04:55 -0500

    Mobile design

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,32 @@
-import { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
 import "./NavBar.css";
+import { Link, useLocation } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import { useState, useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 export default function NavBar() {
 	const location = useLocation();
 	const isHomePage = location.pathname === "/";
 	const isAboutPage = location.pathname === "/about";
 	const isProjectPage = location.pathname === "/projects";
+
+	const [offCanvasNav, setOffCanvasNav] = useState(false);
+	const offcanvasRef = useRef();
+
+	const isMobile = useMediaQuery({ query: "(max-width: 576px)" });
+
+	function handleToggle() {
+		if (isMobile) {
+			setOffCanvasNav(!offCanvasNav);
+		}
+	}
+	function closeOffCanvasNav() {
+		setOffCanvasNav(false);
+	}
+
 	return (
 		<>
 			<Navbar expand="sm" className="navbar">
@@ -24,25 +41,57 @@ export default function NavBar() {
 							Zachary Swan Mitchell
 						</Navbar.Brand>
 					</Link>
-					<Nav>
-						<Link to="/" className={`nav-link ${isHomePage ? "active" : ""}`}>
-							Home
-						</Link>
-						&nbsp; | &nbsp;
-						<Link
-							to="/about"
-							className={`nav-link ${isAboutPage ? "active" : ""}`}
-						>
-							About
-						</Link>
-						&nbsp; | &nbsp;
-						<Link
-							to="/projects"
-							className={`nav-link ${isProjectPage ? "active" : ""}`}
-						>
-							Projects
-						</Link>
-					</Nav>
+					<Navbar.Toggle
+						aria-controls={`offcanvasNavbar-expand-sm`}
+						onClick={handleToggle}
+					/>
+					<Navbar.Offcanvas
+						id={`offcanvasNavbar-expand-sm`}
+						aria-labelledby={`offcanvasNavbarLabel-expand-sm`}
+						placement="end"
+						show={offCanvasNav}
+						onHide={closeOffCanvasNav}
+						ref={offcanvasRef}
+					>
+						<Offcanvas.Header closeButton>
+							<Offcanvas.Title id={`offcanvasNavbarLabel-expand-sm`}>
+								ZSM
+							</Offcanvas.Title>
+						</Offcanvas.Header>
+						<Offcanvas.Body>
+							<Nav className="flex-grow-1">
+								<Nav.Item>
+									<Link
+										to="/"
+										className={`nav-link ${isHomePage ? "active" : ""}`}
+										onClick={handleToggle}
+									>
+										Home
+									</Link>
+								</Nav.Item>
+								&nbsp; &nbsp;
+								<Nav.Item>
+									<Link
+										to="/about"
+										className={`nav-link ${isAboutPage ? "active" : ""}`}
+										onClick={handleToggle}
+									>
+										About
+									</Link>
+								</Nav.Item>
+								&nbsp; &nbsp;
+								<Nav.Item>
+									<Link
+										to="/projects"
+										className={`nav-link ${isProjectPage ? "active" : ""}`}
+										onClick={handleToggle}
+									>
+										Projects
+									</Link>
+								</Nav.Item>
+							</Nav>
+						</Offcanvas.Body>
+					</Navbar.Offcanvas>
 				</Container>
 			</Navbar>
 		</>
